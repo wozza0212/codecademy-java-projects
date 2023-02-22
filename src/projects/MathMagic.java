792aeaa59a7d3f2790f2c584396acbe5bb910d62
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-02-22 20:54:38 +0000

    finished math magic

--- src/projects/MathMagic.java
+++ src/projects/MathMagic.java
@@ -2,6 +2,19 @@ package projects;
 
 public class MathMagic {
     public static void main(String[] args) {
+        //Refer to this as original number in text
+        int myNumber = 70;
+        int stepOne = myNumber*myNumber;
+        int stepTwo = stepOne - myNumber;
+        int stepThree = stepTwo / myNumber;
+        int stepFour = stepThree + 17;
+        int stepFive = stepFour - myNumber;
+        int stepSix = stepFive / 6;
+        System.out.println(stepSix);
+
+
+
+
 
     }
 }
